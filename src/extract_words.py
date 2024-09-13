ec58174df5479c257aa5f44a5f230c49a12d2d61
sg9372 <143601864+sg9372@users.noreply.github.com>
2024-09-13 13:14:47 +0100

    Added words and frequencies to sql database

--- src/extract_words.py
+++ src/extract_words.py
@@ -1,8 +1,8 @@
 # process_words.py: Processes raw text from "load_book.py" into 
 
-from nltk.tokenize import word_tokenize
 import spacy
 import re
+import sqlite3
     
 def extract_words(raw_text):
     text = raw_text.lower()
@@ -16,6 +16,32 @@ def lemmatize(text):
     nlp = spacy.load("es_core_news_sm")
     doc = nlp(text)
     lemmas = [token.lemma_ for token in doc]
-    print(lemmas)
+    sql_storage(lemmas)
 
-#def sql_storage(tokenised_text):
+def sql_storage(lemmas_list):
+    sqlConnection = sqlite3.connect("spanishWords.db")
+    sqlCursor = sqlConnection.cursor()
+
+    sqlCursor.execute("DROP TABLE IF EXISTS spanishWords")
+    sqlCursor.execute("CREATE TABLE IF NOT EXISTS spanishWords(word TEXT PRIMARY KEY, frequency INTEGER)")
+    # ~584 words per page
+
+    word_count = {}
+    
+    # Collect words and frequencies in memory
+    for lemma in lemmas_list:
+        if lemma in word_count:
+            word_count[lemma] += 1
+        else:
+            word_count[lemma] = 1
+    
+    for word, frequency in word_count.items():
+        sqlCursor.execute("INSERT INTO spanishWords(word, frequency) VALUES(?, ?) "
+                          "ON CONFLICT(word) DO UPDATE SET frequency = frequency + ?",
+                          (word, frequency, frequency))
+    
+    sqlCursor.execute("SELECT * FROM spanishWords")
+    for row in sqlCursor.fetchall():
+        print(row)
+    
+    sqlConnection.close()
